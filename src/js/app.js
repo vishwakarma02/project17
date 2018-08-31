a66bda978f184ff8518c7fca0e36eeecee86d081
Vijay_NB <vijay@netbramha.com>
2018-08-31 21:57:48 +0530

    first version

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 import '../css/main.scss';
 import Sidebar from './sidebar';
 
-window.onload = function(){
+window.onload = function(e){
     Sidebar();
 }
--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -8,12 +8,17 @@ let innerSidebar = function(){
     let el = document.querySelector('.hamburger');
     el.addEventListener('click',function(){
         el.classList.toggle('show');
-        moveMain();
-        openSidebar();
-        fixBody();
+        document.querySelector('.rm-sidebar').classList.toggle('show');
+        document.querySelector('main').classList.toggle('show');
     });
 }
 
-
+window.onclick = function(e){
+    if(e.target.classList.contains('sidebar-closer') && (e.target.parentElement.classList.contains('show'))){
+        document.querySelector('.rm-sidebar').classList.toggle('show');
+        document.querySelector('.hamburger').classList.toggle('show');
+        document.querySelector('main').classList.toggle('show');
+    }
+}
 
 module.exports = Sidebar;
